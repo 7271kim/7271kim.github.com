500d51f8fe440dade2628472fa99c123f42a6f84
김석진 <7271kim@naver.com>
2020-01-07 02:22:51 +0900

    [2020.01.07] - js세팅까지 추가

---
+++ assets/js/plugins/markerwidget.js
@@ -0,0 +1,12 @@
+(function(d, h, m){
+    var js, fjs = d.getElementsByTagName(h)[0];
+    if (d.getElementById(m)){return;}
+    js = d.createElement(h); js.id = m;
+    js.onload = function(){
+        window.makerWidgetComInit({
+        position: "right",          
+        widget: "8mnpqgeqiyruagr3-jgcvvdkqr35fuyox-whwv9asjyonauycg"                
+    })};
+    js.src = "https://makerwidget.com/js/embed.js";
+    fjs.parentNode.insertBefore(js, fjs)
+}(document, "script", "dhm"));
